e40fc7ac888460a001d10d65ad6a266f8ad23a10
kalle söderlund <jens.karsod08@edu.umea.se>
2026-09-17 13:43:48 +0200

    början

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 # Här skriver du ditt textäventyr
+
+
+def spelare():
+    player = input("what is your namn? ")
+    age = int(input("what is yo ålder? "))
+
+    if age > 16:
+        input("gogo gaga du har fel!!!!")
+        spelare()
+
+    elif age > 130:
+        input("du fick en hjärt attack!")
+        spelare()
+
+
+spelare()
