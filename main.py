8dd9b30b3ae10d832b9ca817816186e79251df65
kalle söderlund <jens.karsod08@edu.umea.se>
2026-09-22 11:49:24 +0200

    tisdag v.39

--- main.py
+++ main.py
@@ -1,32 +1,21 @@
 # Här skriver du ditt textäventyr
 import random
 #import verity!
-
-
-def introduction():
+global knife
+knife = True
+# (en gångs saker)
+#-----------------------------------------------------
+def titta_start():
     print("\033c", end="")
-    print("booo")
-
-
-
-
-
-def inventory():
-    print("\033c", end="")
-    print ("ajjj")
-
-
-
-def början():
-    global guld
-    guld = 25
-    print("\033c", end="")
-    print ("du heter", (name), "och du är", (age))
-    print ("du har", (guld),"kr")
-    print ("du har vaknat i ditt hus vad vill du gjöra nu?")
-    input ("skriv HELP om du är fast ")
-
-
+    global rev
+    rev = True
+    global bul
+    bul = 3
+    print("du tittar runt ditt hus och du tar up din pistol/revolver")
+    print("du har", (bul), "skott")
+    fråga()
+#-----------------------------------------------------
+#-----------------------------------------------------
 def spelare():
     print("\033c", end="")
     global age
@@ -51,9 +40,120 @@ def spelare():
 
     else:
         början()
+#-----------------------------------------------------
+#-----------------------------------------------------
+def introduction():
+    print("\033c", end="")
+    input("du är en cowboy i dena värld under 1500 talet i Nordamerica. Du välger skälv vad du heter och   din ålder. du kan skälv välga vad du vill gjöra i dena värld")
+    spelare()
+#-----------------------------------------------------
+#-----------------------------------------------------
+def början():
+    global guld
+    guld = 25
+    print("\033c", end="")
+    print ("du heter", (name), "och du är", (age))
+    print ("Du har", (guld),"pengar.")
+    print ("Du har vaknat i ditt hus vad vill du gjöra nu?")
+    information = input ("Skriv hjälp om du är fast. ")
+    if information == "hjälp":
+        information2 = input("Skriv (värld) för att gå till et annat stäle. "
+        "(rygsäck) för att se vad du har.(titta) om du vill tita runt ditt hus ")
+        if information2 ==  "värld":
+            värld()
+        
+        elif information2 ==  "rygsäck":
+            inventory()
+        
+        elif information2 ==  "titta":
+            titta_start()
+
+        else:
+            input("du skrev något fell")
+            början()
+    elif information ==  "värld":
+        värld()
+
+    elif information ==  "rygsäck":
+        inventory()
+
+    elif information ==  "titta":
+        titta_start()
+    else:
+        input("du skrev något fell")
+        början()
+#-----------------------------------------------------
+
+
+
+# (kommer att envändes igen)
+#-----------------------------------------------------
+def värld():
+    print("\033c", end="")
+
+
+
+    print("slut")
+#-----------------------------------------------------
+#-----------------------------------------------------
+def inventory():
+    print("\033c", end="")
+    print ("du har", (guld),"pengar")
+    if rev == True:
+        print("du har en revolver med", (bul),"skot")
+    else:
+        fråga
+#-----------------------------------------------------
+#-----------------------------------------------------
+def fråga():
+    print ("vad vill du gjöra nu?")
+    information = input("Skriv hjälp om du är fast. ")
+    if information == "hjälp":
+        information2 = input("Skriv (värld) för att gå till et annat stäle. "
+        "(rygsäck) för att se vad du har. ")
+        if information2 ==  "värld":
+            värld()
+        
+        elif information2 ==  "rygsäck":
+            inventory()
+        else:
+            input("du skrev något fel")
+            fråga()
+
+    
+        
 
         
-spelare()
+    elif information ==  "värld":
+        värld()
+
+    elif information ==  "rygsäck":
+        inventory()
+    else:
+        print("du skrev något fel")
+        fråga()
+#-----------------------------------------------------
+#-----------------------------------------------------
+def stats():
+    print("<(*)>-------------------------------------<(*)>")
+    print("namn = ", (name), "ålder = ", (age))
+    print("du har", (guld), "pengar")
+    if rev == True:
+        print ("du har en revolver med", (bul))
+    if knife == True:
+        print ("du har en kniv")
+#-----------------------------------------------------
 
 
 
+
+
+
+
+
+
+
+#started av spelet
+#-----------------------------------------------------
+introduction()
+#-----------------------------------------------------
--- textaventyr.kanban.md
+++ textaventyr.kanban.md
@@ -10,17 +10,14 @@
 <!-- id: task-1789371029039-86 -->
 <!-- priority: critical -->
 
-#### Spelarnamn
-<!-- id: task-1789370256358-0 -->
-programmet frågar efter spelarens namn och lagrar det i en variabel
-
-#### f-strängar
-<!-- id: task-1789370290018-5 -->
-spelarens namn används i minst tre print()-satser
-
-#### Vägval i följd
-<!-- id: task-1789370317834-14 -->
-minst två if/else-val efter varandra, en fråga, lite berättelse, en ny fråga
+#### världen
+<!-- id: task-1790065515868-200 -->
+gjör klart så att du kan gå runt i the world!
+- [ ] nej
+Tags: `ehh`
+<!-- priority: critical -->
+<!-- workload: extreme -->
+<!-- source: jag -->
 
 #### .lower()
 <!-- id: task-1789370324820-21 -->
@@ -34,10 +31,51 @@ minst två, och de ska vara olika. Slutar alla vägar likadant har spelarens val
 <!-- id: task-1789370399886-49 -->
 vid normala inmatningar. Om du vill så kan du använda mönstret valideraren från kurswebben
 
+#### gjör så man kan slås med folk
+<!-- id: task-1790066427123-296 -->
+<!-- priority: low -->
+
 ## In Progress
+
+#### f-strängar
+<!-- id: task-1789370290018-5 -->
+spelarens namn används i minst tre print()-satser
+
+#### "stats windo"
+<!-- id: task-1790068279498-495 -->
+gjör så man kan kola vad man har liv, guld osv...
+<!-- priority: high -->
+<!-- workload: hard -->
 
 ## Done
 
 #### Klona repot
 <!-- id: task-1789371224632-115 -->
 Klona / forka repot och börja sedan jobba med materialet
+
+#### Spelarnamn
+<!-- id: task-1789370256358-0 -->
+programmet frågar efter spelarens namn och lagrar det i en variabel
+
+#### Vägval i följd
+<!-- id: task-1789370317834-14 -->
+minst två if/else-val efter varandra, en fråga, lite berättelse, en ny fråga
+
+#### titta runt
+<!-- id: task-1790067078153-390 -->
+gjör så att man kan tita runt i starten
+<!-- priority: low -->
+<!-- workload: extreme -->
+
+#### rygsäck
+<!-- id: task-1790066365640-255 -->
+skriv klart rygsäken
+Tags: `ok`
+<!-- priority: low -->
+<!-- due: 3000-03-31 -->
+<!-- source: eh -->
+
+#### fixa så man kan välgo olika saker efter man har gjort något
+<!-- id: task-1790066500889-341 -->
+titteln
+<!-- priority: low -->
